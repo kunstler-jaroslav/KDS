6b65c9a82d95a89d7b578540e0d624630a032f0a
JaKu <jaroslav.kuenstler@siemens.com>
2023-12-23 15:04:07 +0100

    task_3 init

---
+++ task_3/packet.py
@@ -0,0 +1,125 @@
+# Task 3
+from enum import Enum
+import pickle
+import zlib
+import hashlib
+
+
+class PacketTypes(Enum):
+    data = 0
+    start = 1
+    stop = 2
+    name = 3
+    ack = 4
+    nack = 5
+    hash = 6
+
+
+class Packet:
+
+    def __init__(self, packet_type, length, data):
+        self.packet_type = packet_type
+        self.length = length
+        self.data = data
+        #self.id = None
+
+    def get_content(self):
+        return [self.packet_type, self.length, self.data, self.id]
+
+    #def set_id(self, id):
+     #   self.id = id
+
+
+class PacketCreator:
+
+    @staticmethod
+    def start_packet(data):
+        pack = Packet(PacketTypes.start, len(data), None)
+        return pack
+
+    @staticmethod
+    def stop_packet():
+        pack = Packet(PacketTypes.stop, 0, None)
+        return pack
+
+    @staticmethod
+    def data_packet(data):
+        pack = Packet(PacketTypes.data, len(data), data)
+        return pack
+
+    @staticmethod
+    def name_packet(data):
+        pack = Packet(PacketTypes.name, len(data), data)
+        return pack
+
+    @staticmethod
+    def ack_packet():
+        pack = Packet(PacketTypes.ack, 0, None)
+        return pack
+
+    @staticmethod
+    def nack_packet():
+        pack = Packet(PacketTypes.nack, 0, None)
+        return pack
+
+    @staticmethod
+    def __get_packet_type_by_number(number):
+        for packet_type in PacketTypes:
+            if packet_type.value == number:
+                return packet_type
+        raise ValueError(f"No matching PacketType for number {number}")
+
+    @staticmethod
+    def parse_received(dictionary):
+        pack = Packet(packet_type=dictionary['packet_type'], length=dictionary['length'], data=dictionary['data'])
+        return pack
+
+
+    @staticmethod
+    def hash_packet(file_contents):
+        # TODO calculate hash of file data
+        md5_hash = hashlib.md5(file_contents).hexdigest()
+        pack = Packet(PacketTypes.hash, len(md5_hash), md5_hash)
+        return pack
+
+
+    @staticmethod
+    def get_CRC(data):
+        # Calculate the CRC-32 checksum for the given data
+        crc_value = zlib.crc32(data)
+        # Ensure the result is a positive integer
+        crc_value = crc_value & 0xFFFFFFFF
+        # print(crc_value)
+        hexdump = crc_value.to_bytes(4, byteorder='big')
+        crc_hex = hexdump.rjust(4, b'0')
+        return crc_hex
+
+    @staticmethod
+    def get_hash(data):
+        md5_hash = hashlib.md5(data).hexdigest()
+        return md5_hash
+
+
+class File:
+
+    def __init__(self, name="no_name"):
+        self.name = name
+        self.length = None
+        self.data = b""
+
+    def set_length(self, length):
+        self.length = length
+
+    def add_data(self, data):
+        self.data = self.data + data
+
+    def save_file(self):
+        print(len(self.data))
+        print(self.length)
+        if len(self.data) == self.length:
+            with open(self.name, 'wb') as file:
+                file.write(self.data)
+                file.close()
+            print("File saved successfully")
+        else:
+            print("Error detected, repeat the process")
---
+++ task_3/sender.py
@@ -0,0 +1,139 @@
+# Task 3
+import socket
+import pickle
+import time
+
+from packet import Packet, PacketTypes, PacketCreator
+import os
+
+
+class Sender:
+
+    def __init__(self, ip_src, port_src, ip_dst, port_dst):
+        # Sender
+        self.ip_src = ip_src
+        self.port_src = port_src
+
+        # Receiver
+        self.ip_dst = ip_dst
+        self.port_dst = port_dst
+
+        self.s_send = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.s_rec = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.s_rec.bind((self.ip_src, self.port_src))
+        self.s_send.settimeout(3)
+        self.s_rec.settimeout(3)
+        self.packet_id = 0
+
+    @staticmethod
+    def get_local_ip():
+
+        try:
+            # Create a socket object
+            s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            s.settimeout(0.1)
+
+            # Connect to an external server (doesn't actually send data)
+            s.connect(('10.255.255.255', 1))
+
+            # Get the local IP address
+            loc_ip = s.getsockname()[0]
+
+            # Close the socket
+            s.close()
+
+            return str(loc_ip)
+        except Exception as e:
+            print(f"Error getting local IP address: {e}")
+            return None
+
+    def check_ack(self):
+        try:
+            ack, _ = self.s_rec.recvfrom(1024)
+            CRC_ack, serialized_ack = ack[:4], ack[4:]
+            if CRC_ack != PacketCreator.get_CRC(serialized_ack):
+                print("Ack CRC mismatch")
+                return False
+                # Deserialize Ack packet sent from receiver:
+            received_ack = pickle.loads(serialized_ack)
+            pack = PacketCreator.parse_received(received_ack.__dict__)
+            if pack.packet_type == PacketTypes.nack:
+                print("Received negative acknowledgement, sending packet again..")
+                return False
+            if pack.packet_type == PacketTypes.ack:
+                print("Ack received")
+                self.packet_id += 1
+                return True
+        except socket.timeout:
+            print("Timeout. Sending packet again.")
+            return False
+
+    def __send_packet_with_ack(self, packet):
+        self.__send_packet(packet)
+        while not self.check_ack():
+            self.__send_packet(packet)
+
+    def __send_packet(self, packet):
+        try:
+            # Serialize the Packet object using pickle
+            serialized_packet = pickle.dumps(packet)
+            crc = PacketCreator.get_CRC(serialized_packet)
+            hexdump = self.packet_id.to_bytes(1, byteorder='big')
+            id = hexdump.rjust(1, b'0')
+            # print("Serialized packet:", serialized_packet)
+            # print("Crc:", crc)
+            print("{} sent to {}:{}".format(len(crc + id + serialized_packet), self.ip_dst, self.port_dst))
+
+            # Send the data to the receiver's address
+            self.s_send.sendto(crc + id + serialized_packet, (self.ip_dst, self.port_dst))
+
+        except Exception as e:
+            print(f"Error sending packet: {e}")
+
+    def send_file(self, data, file_name):
+        # Send name
+        packet_to_send = PacketCreator.name_packet(file_name)
+        self.__send_packet_with_ack(packet_to_send)
+        # Send start
+        packet_to_send = PacketCreator.start_packet(data)
+        self.__send_packet_with_ack(packet_to_send)
+        # Send file data
+        blocks = [data[i:i + 1024 - 108] for i in range(0, len(data), 1024 - 108)]
+        total = len(data)
+        sent = 0
+        for i in range(len(blocks)):
+            packet_to_send = PacketCreator.data_packet(blocks[i])
+            self.__send_packet_with_ack(packet_to_send)
+            sent += len(blocks[i])
+            print("sent {}%".format((sent / total * 100).__round__(2)), "packet id:", self.packet_id)
+            os.system('cls' if os.name == 'nt' else 'clear')
+        # Send file hash
+        packet_to_send = PacketCreator.hash_packet(data)
+        self.__send_packet_with_ack(packet_to_send)
+        # Send stop
+        packet_to_send = PacketCreator.stop_packet()
+        self.__send_packet_with_ack(packet_to_send)
+
+    @staticmethod
+    def file_to_byte_string(file_path):
+        with open(file_path, 'rb') as file:
+            byte_string = file.read()
+        return byte_string
+
+
+if __name__ == "__main__":
+    # Sender
+    # local_ip = Sender.get_local_ip()
+    local_ip = "127.0.0.1"
+    local_port = 15001  # 50001
+
+    # Receiver
+    destination_ip = "127.0.0.1"
+    destination_port = 14000
+
+    bt = Sender.file_to_byte_string(r"/home/frederik/Desktop/dog.jpg")
+    fn = "dog.jpg"
+
+    if local_ip:
+        sender = Sender(local_ip, local_port, destination_ip, destination_port)
+        sender.send_file(bt, file_name=fn)
